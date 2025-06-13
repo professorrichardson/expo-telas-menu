e74b48f6833509aea81d6e5fa1debe528b729866
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-06-13 14:46:22 +0000

    Fix files

--- App.js
+++ App.js
@@ -5,16 +5,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Alura!</Text>
+      <Text style={styles.texto}>Prof.Richardson</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  texto:{
+    color: '#fff',
+    fontSize: 40
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
